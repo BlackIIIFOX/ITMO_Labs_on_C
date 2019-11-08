5c74ebeb186533b799a317a2d6ea0cb69d065161
Paul Rozhkin <blackiiifox@gmail.com>
2019-11-09 02:19:05 +0300

    Add lab3. Not my.

---
+++ Lab3/settings.h
@@ -0,0 +1,11 @@
+#ifndef CLIENT_SERVER_TCP_STATE_H
+#define CLIENT_SERVER_TCP_STATE_H
+
+#define TCP_PORT 11233
+#define SOCKET_PROTOCOL 0
+#define FLAG 0
+#define STD_ALLOCATE_COUNT 128
+
+extern int make_iso_compilers_happy;
+
+#endif
